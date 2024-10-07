c85bc3205d0b8ab7b3f550c089ad50e8f26a79e4
alice20240103 <alice20240103@gmail.com>
2024-10-07 10:51:17 +0900

    first

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookService service;
+	
+	@GetMapping("/book/insert")
+	public void insert() {		
+	}
+	
+	@PostMapping("/book/insert")
+	public String insertSumbit(Book book) {		
+		service.save(book);
+		return "redirect:/book/list";
+	}
+	
+	@GetMapping("/book/list")
+	public void list(Model model) {
+		model.addAttribute("list", service.findAll());
+	}
+	
+	
+}
